5bac8309a011059c70e4cd879583d3f56854eeb6
Clint Losee <clint.losee@gmail.com>
2017-04-25 14:21:38 -0600

    Added data update capability to manager app.

---
+++ manager/src/components/EmployeeEdit.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+// import { employeeUpdate, employeeCreate } from '../actions';
+import EmployeeForm from './EmployeeForm';
+import { employeeUpdate, employeeSave } from '../actions';
+import { Card, CardSection, Button } from './common';
+
+class EmployeeEdit extends Component {
+    componentWillMount() {
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
+
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+        console.log(name, phone, shift);
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
+    }
+
+    render() {
+        return (
+            <Card>
+                <EmployeeForm />
+                <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this)}>
+                        Save Changes
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
