7249c003d7d4d1db911285d335321a62d1062d75
Clint Losee <clint.losee@gmail.com>
2017-05-02 11:25:16 -0600

    Added swipe rotation to cards in swipe app.

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
     constructor(props) {
@@ -12,11 +14,30 @@ class Deck extends Component {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
             onPanResponderRelease: () => {
-
+                this.resetPosition();
             }
         });
 
         this.state = { panResponder, position };
+    }
+
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 }
+        }).start();
+    }
+
+    getCardStyle() {
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+        }
     }
 
     renderCards() {
@@ -25,7 +46,7 @@ class Deck extends Component {
                 return ( 
                     <Animated.View
                         key={item.id}
-                        style={this.state.position.getLayout()}
+                        style={this.getCardStyle()}
                         {...this.state.panResponder.panHandlers}
                     >
                         {this.props.renderCard(item)}
