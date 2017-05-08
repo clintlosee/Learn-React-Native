064a95965bead76b591d48c83bb1b3759637fc0b
Clint Losee <clint.losee@gmail.com>
2017-05-08 08:44:45 -0600

    Adding stacking of cards and finalized swipe app.

--- swipe/main.js
+++ swipe/main.js
@@ -65,7 +65,8 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    marginTop: 10
   }
 });
 
