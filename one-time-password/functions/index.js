a258f40aedb4520bd14d81e409389bd68e453efa
Clint Losee <clint.losee@gmail.com>
2017-05-15 11:54:13 -0600

    Adding one-time-password project with some initial examples and setup.

---
+++ one-time-password/functions/index.js
@@ -0,0 +1,11 @@
+var functions = require('firebase-functions');
+var createUser = require('./create_user');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.createuser = functions.https.onRequest(createUser);
