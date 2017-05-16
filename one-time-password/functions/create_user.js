e5cb3d85a4edd9cfd2c3fcf9e97ac1c1014808af
Clint Losee <clint.losee@gmail.com>
2017-05-16 09:45:06 -0600

    Set up user creation cloud function for one-time-password app.

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,19 @@
-module.exports = function(req, res) {
+const admin = require('firebase-admin');
 
+module.exports = function(req, res) {
+    // Verify there is a phone number
+    if (!req.body.phone) {
+        return res.status(422).send({ error: 'Bad Input'});
+    }
+
+    // Format phone number to remove digits
+    const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+    // Create a new user from the phone number entered
+    admin.auth().createUser({ uid: phone })
+        .then(user => res.send(user))
+        .catch(err => res.status(422).send({ error: err }));
+
+    // Respond back that user was created
+    
 };
--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,11 +1,11 @@
+const admin = require('firebase-admin');
 var functions = require('firebase-functions');
 var createUser = require('./create_user');
+const serviceAccount = require('./serviceAccount.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-81201.firebaseio.com"
 });
 
 exports.createuser = functions.https.onRequest(createUser);
