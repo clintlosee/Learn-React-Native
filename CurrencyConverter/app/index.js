29c7808ed311934a906535b2816e0d3f41d4cf8d
Clint Losee <clint.losee@gmail.com>
2018-03-28 15:45:01 -0600

    Set up error notifications. Set up navigation between screens for options and currency check.

--- CurrencyConverter/app/index.js
+++ CurrencyConverter/app/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-// import Home from './screens/Home';
-// import CurrencyList from './screens/CurrencyList';
-// import Options from './screens/Options';
-import Themes from './screens/Themes';
+
+import Navigator from './config/routes';
+import { AlertProvider } from './components/Alert';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -17,4 +16,4 @@ EStyleSheet.build({
   $darkText: '#343434',
 });
 
-export default () => <Themes />;
+export default () => <AlertProvider><Navigator /></AlertProvider>;
--- CurrencyConverter/app/screens/Options.js
+++ CurrencyConverter/app/screens/Options.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import PropTypes from 'prop-types';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_COLOR = '#868686';
 const ICON_SIZE = 23;
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 
 class Options extends Component {
+  static propTypes = {
+    navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
+  };
+
   handleThemesPress = () => {
-    console.log('handle themes press');
+    this.props.navigation.navigate('Themes', { title: 'Themes' });
   };
 
   handleSitePress = () => {
-    console.log('handle site press');
+    Linking.openURL('http://fixer.io').catch(() =>
+      this.props.alertWithType('error', 'Sorry!', "Fixer.io can't be opened right now."),
+    );
   };
 
   render() {
@@ -39,4 +48,4 @@ class Options extends Component {
   }
 }
 
-export default Options;
+export default connectAlert(Options);
